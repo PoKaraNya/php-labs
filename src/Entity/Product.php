7d8d2dabaa29b6bf00bd0c93ca11fab456df42af
Kara <zbunkarina@gmail.com>
2024-11-21 11:39:00 +0200

    problem solved

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -15,6 +16,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  */
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ApiResource]
 class Product implements JsonSerializable
 {
     /**
